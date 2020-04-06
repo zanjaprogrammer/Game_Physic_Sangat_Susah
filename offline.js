05d65922594b0a29c4c868fe86f386861269803d
kahfi <azanjabiil@gmail.com>
2020-04-06 12:51:30 +0700

    Game Physic

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1586152160,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"images/particles.png",
+		"images/sprite12-sheet0.png",
+		"images/sprite18-sheet0.png",
+		"images/sprite19-sheet0.png",
+		"images/sprite20-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
